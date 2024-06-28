d4274d05190efaa67d7b44f3f639f42ffa5fac5e
Grandkojo <essienernest.kojoowus@gmail.com>
2024-06-28 00:54:40 +0000

    error handling with the user complete

--- api/views/user.py
+++ api/views/user.py
@@ -23,6 +23,7 @@ def home():
 def signup():
     from api.views.db import Users
     from app import db
+    error = None
     if request.method == 'POST':
         form_data = request.form
         user_data = {
@@ -30,14 +31,23 @@ def signup():
             'password': form_data['user_password'],
             'user_name': form_data['user_name']
         }
-        new_user = Users(username=user_data.get("user_name"), email=user_data.get("email"))
-        new_user.set_password(user_data.get("password"))
-        db.session.add(new_user)
-        db.session.commit()
+        email = Users.query.with_entities(Users.email).filter_by(email=user_data.get("email")).scalar()
 
-        session['user'] = user_data
-        return redirect(url_for('user_bp.signup_complete'))
-    return render_template('signup.html', session=session.get("user"), pretty=json.dumps(session.get("user"), indent=4))
+        user_name = Users.query.with_entities(Users.username).filter_by(username=user_data.get("username")).scalar()
+        if email == user_data.get("email"):
+            error = "Email already exists, login instead"
+        elif user_name == user_data.get("user_name"):
+            error = "Username already exists, try another"
+        else:
+            new_user = Users(username=user_data.get("user_name"), email=user_data.get("email"))
+            new_user.set_password(user_data.get("password"))
+            db.session.add(new_user)
+            db.session.commit()
+            
+            session['user'] = user_data
+            return redirect(url_for('user_bp.signup_complete'))
+        return render_template('signup.html', error=error)
+    return render_template('signup.html')
 
 @user_blueprint.route('/signup-complete')
 def signup_complete():
@@ -53,6 +63,7 @@ def login():
 @user_blueprint.route("/quizzerz-login", methods=["POST", "GET"])
 def quizzerz_login():
     from api.views.db import Users
+    error = None
     if request.method == "POST":
         form_data = request.form
         user_data = {
@@ -62,10 +73,12 @@ def quizzerz_login():
         user = Users.query.filter_by(email=user_data.get("email")).first()
         if user and user.check_password(user_data.get("password")):
             session['user'] = user_data
+            flash("You were succesfully logged in")
             return redirect(url_for('user_bp.home', method="quizzerz"))
         else:
-             flash("Invalid credentials", 'error')
-    return redirect(url_for("user_bp.login"))
+             error = "Invalid email or password"
+        return render_template("login.html", error=error)
+    return render_template("login.html")
 
 @user_blueprint.route("/google-login")
 def google_login():
